71a5f0a02cbc27b586982fa4daa683e4c38bfc37
KevinChiguano <kevin_chili1998@hotmail.com>
2024-08-11 23:49:18 -0500

    cambios

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    proxy: {
-      '/apiAutor': {
-        target: 'http://localhost:9090',
-        changeOrigin: true,
-        secure: false,
-        pathRewrite: { '^/apiAutor': '' },
-      },
-      '/apiLibro': {
-        target: 'http://localhost:8090',
-        changeOrigin: true,
-        secure: false,
-        pathRewrite: { '^/apiLibro': '' },
-      },
-    },
-  },
 })
